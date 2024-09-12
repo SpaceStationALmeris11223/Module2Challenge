db3002e914d96c41ed2e717cce31d30da5ff5c10
SpaceStationALmeris11223 <125327160+SpaceStationALmeris11223@users.noreply.github.com>
2024-09-12 00:40:12 +0000

    Challege 2

---
+++ Module2Challenge/Program.cs
@@ -0,0 +1,20 @@
+﻿// Welcome the use to the program
+Console.WriteLine("Welcome to the miles per gallon calculator!");
+
+//Declaring the variables
+double milesDriven;
+double gallonsUsed;
+
+//Ask the user how many miles they drove
+Console.WriteLine("How Many Miles long was your trip?");
+milesDriven = double.Parse(Console.ReadLine());
+//Ask the user how many gallons of gas they used
+Console.WriteLine("How many gallons of gas did you use for this trip?");
+gallonsUsed = double.Parse(Console.ReadLine());
+
+//calculate Their Miles Per Gallon 
+double mpg = milesDriven / gallonsUsed;
+double roundedMPG = Math.Round(mpg, 2);
+//output the results 
+Console.WriteLine($"you drove {milesDriven} and burned {gallonsUsed} of gas during this trip. Your Miles per gallon is {roundedMPG} ");
+
